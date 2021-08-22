9689545ef96da396da90c4e1eb7f86864f6fa27a
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-22 17:08:34 -0300

    add firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,12 +7,28 @@ const config: NuxtConfig = {
 		'@nuxtjs/google-fonts',
 	],
 	components: true,
+	firebase: {
+		config: {
+			apiKey: 'AIzaSyDvOb2CGC_T52YRS_lAHTBXTt1vcT7y_Bk',
+			authDomain: 'english-irregular-verbs-list.firebaseapp.com',
+			projectId: 'english-irregular-verbs-list',
+			storageBucket: 'english-irregular-verbs-list.appspot.com',
+			messagingSenderId: '167135621400',
+			appId: '1:167135621400:web:080662303ab9a7f7e42e6d',
+			measurementId: 'G-Z3CD249ZE9',
+		},
+		services: {
+			analytics: true,
+			firestore: true,
+		},
+	},
 	googleFonts: {
 		display: 'swap',
 		families: {
 			Rubik: [400, 500, 700],
 		},
 	},
+	modules: ['@nuxtjs/firebase'],
 	srcDir: 'src',
 	target: 'static',
 }
