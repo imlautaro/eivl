33ece8bbd4fc57688a7fc000cb0d6d55a1907d04
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-22 16:12:44 -0300

    add google-fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
-	buildModules: ['@nuxt/typescript-build', 'nuxt-windicss'],
+	buildModules: [
+		'@nuxt/typescript-build',
+		'nuxt-windicss',
+		'@nuxtjs/google-fonts',
+	],
 	components: true,
+	googleFonts: {
+		display: 'swap',
+		families: {
+			Rubik: [400, 500, 700],
+		},
+	},
 	srcDir: 'src',
 	target: 'static',
 }
---
+++ windi.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'windicss/helpers'
+
+const config = defineConfig({
+	theme: {
+		fontFamily: {
+			sans: ['Rubik', 'sans-serif'],
+		},
+	},
+})
+
+export default config
