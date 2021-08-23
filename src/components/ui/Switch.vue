b5b49acb1bd894ba44c19b25679718880fcca719
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-23 12:01:31 -0300

    fix switch active background in dark mode

--- src/components/ui/Switch.vue
+++ src/components/ui/Switch.vue
@@ -2,11 +2,7 @@
 	<button
 		@click="$emit('change', !active)"
 		class="duration-200 p-1 rounded-full w-12"
-		:class="
-			active
-				? 'bg-primary-600 dark:bg-primary-800'
-				: 'bg-gray-400 dark:bg-gray-600'
-		"
+		:class="active ? 'bg-primary-600' : 'bg-gray-400 dark:bg-gray-600'"
 	>
 		<div
 			class="duration-200 ball transition-all bg-white shadow-md w-4 h-4 rounded-full transform"
