9f27d0dc2deb1deff4a0d6f4160302773d2b58a1
Lautaro Pereyra <hi@imlautaro.com>
2021-10-19 08:14:40 -0300

    add query cleaner

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -11,12 +11,20 @@
 			placeholder="Search..."
 			v-model="query"
 		/>
+		<button
+			v-if="query.length"
+			@click.prevent="query = ''"
+			class="flex p-4"
+		>
+			<ph-x-bold />
+		</button>
 	</div>
 </template>
 
 <script setup>
 import useSearch from '~/composables/search'
 import PhMagnifyingGlassBold from '~icons/ph/magnifying-glass-bold'
+import PhXBold from '~icons/ph/x-bold'
 
 const { query } = useSearch()
 </script>
